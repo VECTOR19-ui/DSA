a91df1646b7dad91dded77f1bcad620626e52239
Yordanos Asegiedew <103318194+VECTOR19-ui@users.noreply.github.com>
2023-09-27 21:48:21 +0300

    Add files via upload

---
+++ Data Structure/Array/ARRAY TRAVERSAL.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+	int LA[5]={10,20,30,40,50};
+	int length=5;
+	for(int i=0;i<5;i++){
+	    LA[length-1];
+	    printf("Array element:[%d]\n",LA[i]);
+	}
+	return 0;
+}
